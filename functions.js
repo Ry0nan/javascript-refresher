8f9e94d4fd6bba83411c981516ff50fbf4478c54
Ryonan <125460295+Ry0nan@users.noreply.github.com>
2025-05-13 18:16:54 +0800

    learned about functions

---
+++ functions.js
@@ -0,0 +1,19 @@
+function greet(name) {
+    console.log(name)
+}
+
+greet("Ryonan")
+
+const greetUser = function(name) {
+
+}
+
+//arrow functions
+
+const greetUser2 = (name) => name + "!";
+
+greetUser2("Abelardo");
+
+function addNums(...numbers){
+
+}
